cd72dd8365b9d826c40d64d66c406a1a4e8f6a58
papistan <jpapisan@gmail.com>
2017-09-08 22:25:53 -0700

    Add active deck view reducer

--- src/containers/card_view.js
+++ src/containers/card_view.js
@@ -7,20 +7,25 @@ class CardView extends Component {
 		if (prop == null) {
 					
 						return (
-							<div>
-								<h3>Select a topic</h3>
+							<div className="video-detail col-md-8">
+								<div className="embed-responsive embed-responsive-16by9">
+									
+									<img src="http://blog.aurionlearning.com/wp-content/uploads/2016/11/html-css-js-1.png" />
+									<h3>Select a topic => </h3>
+								</div>
 							</div>
 						)
 					
 		} else { 
 				
 					return (
-						<div>
-							<h3>Details for:</h3>
-							<div>{this.props.deck.title}</div>
+						<div className="video-detail col-md-8">
+							<div className="embed-responsive embed-responsive-16by9">
+								<h3>Details for:</h3>
+								<img src={this.props.deck.image} />
+							</div>
 						</div>
 					)
-
 		}
 	}
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import DecksReducer from './reducer_decks';
+import ActiveDeck from './reducer_active_deck';
 
 const rootReducer = combineReducers({
-  decks: DecksReducer
+  decks: DecksReducer,
+  activeDeck: ActiveDeck
 });
 
 export default rootReducer;
