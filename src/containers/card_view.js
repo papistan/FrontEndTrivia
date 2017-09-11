c28b8b813f43142867517c1666cda2853a323cf2
papistan <jpapisan@gmail.com>
2017-09-10 22:27:23 -0700

    update buttons and refactor card view functions

--- src/containers/card_view.js
+++ src/containers/card_view.js
@@ -6,14 +6,21 @@ class CardView extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { currentCard: 0 };
+		this.state = { currentCard: "", cardsArray: [] };
 	}
-	
+
 	changeCard() {
-				this.setState({
-					currentCard: this.state.currentCard + 1
-				});
-	}	
+		this.state.cardsArray.push(cardsArray.shift());
+		this.setState({
+			currentCard: this.state.cardsArray[0]
+		});
+	};
+
+	setCard() {
+		this.setState({
+			currentCard: this.state.cardsArray[0]
+		});
+	};
 
 	render(){
 		var chosenDeck = this.props.deck
@@ -32,27 +39,35 @@ class CardView extends Component {
 		} else { 
 		
 			
-
-			const cards = chosenDeck.cards.map((card) => {
+		const cardsArray = chosenDeck.cards.map((card) => {
 				return ( 
 					<Card 
 						key={card.q}
 						card={card}
 					/>
 				)
-			})
-			
-			return (
-						<div className="col-md-4 list-group card-view">
-							<button 
-							type="button"
-							onClick={this.changeCard}
-							/>
+			});
+			// console.log(this.state.currentCard)
+			// console.log("************")
 
-							<div>{cards[this.state.currentCard]}</div>
+			return (
+					<div>
+						<div className="col-md-4 list-group card-view">
+							<div>
+								<button 
+								className="button button1"
+								onClick={this.changeCard}
+								>NEXT</button>
+								<button 
+								className="button button2"
+								onClick={this.changeCard}
+								>NEXT</button>
+							</div>
+
+							<div>{this.state.currentCard}</div>
 						</div>
 
-						
+						</div>
 
 					)
 		}
