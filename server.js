9434ac294f508941d2c64b1fce3195b39e83bc60
papistan <jpapisan@gmail.com>
2017-09-14 20:11:26 -0700

    bundle

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'index.html'))
+});
+
+app.listen(port);
+console.log('server started');
