856ef95933e3e2e1816afb5302e2896730ad6284
papistan <jpapisan@gmail.com>
2017-10-17 20:49:58 -0700

    refactor for formatting

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import DeckList from '../containers/deck_list';
-import CardView from '../containers/card_view';
+import React, { Component } from "react";
+import DeckList from "../containers/deck_list";
+import CardView from "../containers/card_view";
 
 export default class App extends Component {
   render() {
     return (
-    	<div className="app">
-      	<div className="title-app">
-      		<h1 id="title">Front End Trivia</h1>
-      	</div>
-      	<div className="content">
-	      	<CardView className="card-view-app"/>
-	      	<DeckList className="deck-list-app"/>
-      	</div>
+      <div className="app">
+        <div className="title-app">
+          <h1 id="title">Front End Trivia</h1>
+        </div>
+        <div className="content">
+          <CardView className="card-view-app" />
+          <DeckList className="deck-list-app" />
+        </div>
       </div>
     );
   }
