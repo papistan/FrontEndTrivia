bf6eaca4eb169c72c12703fbe3201487cc5f3086
Matheus da Costa Mesquita <matheus.mesquita@b2wdigital.com>
2017-10-28 10:52:49 -0200

    add question (What is the specificity of '.classA.classB #idA > ul ~ ul.classC' ?)

--- src/data/file.js
+++ src/data/file.js
@@ -939,6 +939,11 @@ export function deckData() {
           q: 'What does the CSS property "box-sizing":"border-box" do?',
           a:
             "It alters the default CSS box model used to calculate width and height of the elements to include any border and padding in the value you specify for width and height."
+        },
+        {
+          q: "What is the specificity of '.classA.classB #idA > ul ~ ul.classC' ?",
+          a:
+            "0(inline style) 1(ID selectors) 3(class selectors) 2(element selectors)"
         }
       ]
     },
